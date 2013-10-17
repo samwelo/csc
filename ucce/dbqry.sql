ce76ce7948a992a967766dffa8e208949024d6ff
Sam <samwelo@outlook.com>
2013-10-17 15:53:22 -0500

    Create dbqry.sql

---
+++ ucce/dbqry.sql
@@ -0,0 +1,7 @@
+select * from Termination_Call_Detail where DateTime > '10/1/11 00:01:00’
+
+select * from Termination_Call_Detail when ANI = '6126367614'
+select * from Route_Call_Detail where DateTime > '10/1/11 00:01:00’
+select * from Termination_Call_Detail when ANI = '6126367614'
+select * from Route_Call_Variable where DateTime > '10/1/11 00:01:00’
+select * from Termination_Call_Detail when ANI = '6126367614'
