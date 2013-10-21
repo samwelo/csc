de6cbe55fd48f3e15f1b314c49d1f45c50da9a98
Sam <samwelo@outlook.com>
2013-10-21 11:05:57 -0500

    Update debug.tcl

--- debug/debug.tcl
+++ debug/debug.tcl
@@ -3,6 +3,24 @@ sh ip int brief
 
 debug isdn status
 
+#view if the PRI is up/down
+Show isdn status
 
-/**test*/
-show
+#see which channels are in use
+Show isdn service
+
+#see calls coming into the gateway
+Debug isdn q931
+
+#see what dial peer was used
+Debug voip dialpeer
+
+#see if conference and transcoders 
+#are registered and check in Cm also 
+#to verify that the Conf Bridge and Transcoders 
+#are registered with GWY IP
+Show sccp
+
+#see if the gateways resources are registered
+Show dspfarm
+
